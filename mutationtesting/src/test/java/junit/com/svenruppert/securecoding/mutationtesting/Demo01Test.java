22b6b1534f746419849b795431c1c7a18c8d791f
Claudia <claudia@maderthaner.org>
2025-03-12 14:36:03 +0100

    Fix test name pattern

--- mutationtesting/src/test/java/junit/com/svenruppert/securecoding/mutationtesting/Demo01Test.java
+++ mutationtesting/src/test/java/junit/com/svenruppert/securecoding/mutationtesting/Demo01Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class DemoTest01 {
+class Demo01Test {
 
   Demo demo;
 
