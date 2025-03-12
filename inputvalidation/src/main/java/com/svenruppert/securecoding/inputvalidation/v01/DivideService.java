9d2dc52f995d81f7d961b9544daca348215e7ce5
Sven Ruppert <sven.ruppert@gmail.com>
2025-03-12 16:14:29 +0100

    input validation v01 - DivideService.java

--- inputvalidation/src/main/java/com/svenruppert/securecoding/inputvalidation/v01/DivideService.java
+++ inputvalidation/src/main/java/com/svenruppert/securecoding/inputvalidation/v01/DivideService.java
@@ -2,7 +2,8 @@ package com.svenruppert.securecoding.inputvalidation.v01;
 
 public class DivideService {
 
-  public float divide(float a, float b){
+  public float divide(float a, float b) {
+    if(b == 0) throw new IllegalArgumentException("alles doof");
     return a / b;
   }
 
