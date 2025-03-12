63a675a417a9a06d1373d96a15d4ea2a78c69727
nemesisxnc <nemesisxnc@web.de>
2025-03-12 14:27:27 +0100

    Create Demo04Test.java

---
+++ mutationtesting/src/test/java/junit/com/svenruppert/securecoding/mutationtesting/Demo04Test.java
@@ -0,0 +1,20 @@
+package junit.com.svenruppert.securecoding.mutationtesting;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.svenruppert.securecoding.mutationtesting.Demo;
+
+class DemoTest {
+
+    @Test
+    void test001() {
+        var demo = new Demo();
+        int result = demo.add(1, 5);
+        assertEquals(-6, result);
+        result = demo.add(2, 5);
+        assertEquals(7, result);
+        result = demo.add(3, 5);
+        assertEquals(8, result);
+    }
+}
