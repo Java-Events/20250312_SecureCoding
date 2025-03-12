f37fd4a704479acf1f8ed6ac9511e3ec0d07f09e
Janne Ingo Lange <janne_ingo.lange@hsbi.de>
2025-03-12 16:40:43 +0100

    Fehlende Leerzeichen hinzugefügt nach if

--- inputvalidation/src/main/java/com/svenruppert/securecoding/inputvalidation/v01/DivideService.java
+++ inputvalidation/src/main/java/com/svenruppert/securecoding/inputvalidation/v01/DivideService.java
@@ -5,12 +5,12 @@ import java.util.Optional;
 public class DivideService {
 
   public float divide(float a, float b) {
-    if(b == 0) throw new IllegalArgumentException("Div durch null geht nicht");
+    if (b == 0) throw new IllegalArgumentException("Div durch null geht nicht");
     return a / b;
   }
 
   public Optional<Float> divideNull(float a, float b) {
-    if(b == 0) return  Optional.empty();
+    if (b == 0) return  Optional.empty();
     return Optional.of(a / b);
   }
 
