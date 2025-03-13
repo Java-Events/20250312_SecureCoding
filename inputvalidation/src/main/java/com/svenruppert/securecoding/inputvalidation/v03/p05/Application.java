ea1c5e6278385eac54824a301f53b6d8dc1252c3
Janne Ingo Lange <janne_ingo.lange@hsbi.de>
2025-03-13 10:54:21 +0100

    Package p05 initial erstellt; Server und Client lauffähig

---
+++ inputvalidation/src/main/java/com/svenruppert/securecoding/inputvalidation/v03/p05/Application.java
@@ -0,0 +1,10 @@
+package com.svenruppert.securecoding.inputvalidation.v03.p05;
+
+public class Application {
+
+  public static void main(String[] args) {
+    // port bestimmen
+    // service Instance starten
+    //
+  }
+}
---
+++ inputvalidation/src/main/java/com/svenruppert/securecoding/inputvalidation/v03/p05/handler/DemoHandler.java
@@ -0,0 +1,4 @@
+package com.svenruppert.securecoding.inputvalidation.v03.p05.handler;
+
+public class DemoHandler {
+}
